e37539ec93276ba0023c72984621e9f6ed28bd7c
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-03-06 00:02:21 +0900

    first commit

---
+++ blog_project/blog/models.py
@@ -0,0 +1,16 @@
+from django.urls import reverse
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
